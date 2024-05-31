7573fb47c5175a9da55492c97467689492555b1c
okore-lens <okorelens@gmail.com>
2024-05-31 22:47:25 +0300

    core: hosting config

--- src/views/components/hero-section.tsx
+++ src/views/components/hero-section.tsx
@@ -1,20 +1,13 @@
 import { H1, Subtitle2 } from "@components/typography";
 import { Button } from "@nextui-org/react";
-import clsx from "clsx";
 
 import MissionCard from "./mission-card";
 import { Link } from "react-router-dom";
 
 const HeroSection = () => {
 	return (
-		<section
-			className={clsx(
-				"min-h-[132vh] md:min-h-[114vh] lg:min-h-[125vh]  !mb-10",
-				innerHeight < 800 && innerWidth < 450 && "!min-h-[150vh]",
-				innerHeight < 700 && innerHeight < 400 && "!mb-40"
-			)}
-		>
-			<section className="h-[90vh] relative flex items-center justify-center flex-col text-center bg-heroImage bg-center bg-cover bg-blend-multiply  bg-black-600">
+		<section>
+			<section className="min-h-[60vh] lg:min-h-[80vh]  flex items-center justify-center flex-col text-center bg-heroImage bg-center bg-cover bg-blend-multiply  bg-black-600">
 				<section className="container">
 					<section className="md:container flex flex-col gap-5 ">
 						<H1 className="text-grayish ">
@@ -37,8 +30,8 @@ const HeroSection = () => {
 						</Button>
 					</section>
 				</section>
-				<MissionCard />
 			</section>
+			<MissionCard />
 		</section>
 	);
 };
--- src/views/components/mission-card.tsx
+++ src/views/components/mission-card.tsx
@@ -2,7 +2,7 @@ import { H2, Paragraph } from "@components/typography";
 
 const MissionCard = () => {
 	return (
-		<section className="container absolute top-[90%] bg-grayish text-center py-20 px-5 md:px-10">
+		<section className="container  bg-grayish text-center py-10 lg:py-20 px-5 md:px-8 lg:px-10 -mt-20 mb-16 z-[999]">
 			<H2>
 				Our <span className="italic">Mission</span> Is Clear
 			</H2>
